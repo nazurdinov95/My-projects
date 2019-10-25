55c91acb39555166aff5c8b4ec2119302698e2d1
bin-umar <bin-umar@mail.ru>
2019-10-25 16:00:34 +0300

    deleted chunks

--- todolist.js
+++ todolist.js
@@ -1,77 +1,47 @@
-
-
-//Close button
-const closeButton = document.getElementsByClassName("close");
-
-for (i = 0; i < closeButton.length; i++){
-    closeButton[i].onclick = function () {
-        const theDiv = this.parentElement;
-        theDiv.style.display = "none";
-    }
-}
+'use strict';
 
 const ulList = document.getElementById('the-ul');
-ulList.addEventListener('click', function(event) {
-    console.log(event);
-    if(event.target.tagName === "LI"){
-        event.target.classList.toggle('checked');
+ulList.addEventListener('click', (e) => {
+    if(e.target.tagName === 'LI') {
+        e.target.classList.toggle('checked');
     }
-},false);
+});
 
 //creating todos function
 function createNewElement() {
-    var li = document.createElement('li');
-    var theInputValue = document.getElementById("the-input").value;
-    var textNode = document.createTextNode(theInputValue);
+    const li = document.createElement('li');
+    const theInputValue = document.getElementById('the-input').value;
+    const textNode = document.createTextNode(theInputValue);
     li.appendChild(textNode);
-
-    function newItem() {
-        var item = document.getElementById("the-input").value;
-        var ul = document.getElementById("the-ul");
-        var li = document.createElement("li");
-        li.appendChild(document.createTextNode(item));
-        ul.appendChild(li);
-        document.getElementById("the-input").value = "";
-    }
-
-    document.body.onkeyup = function(e) {
-        if (e.keyCode == 13) {
-            newItem();
-        }
-    };
-
+    
     if (theInputValue === '') {
-        alert("Hey this cannot be empty")
+        alert('Hey this cannot be empty')
     } else {
-        document.getElementById("the-ul").appendChild(li);
+        document.getElementById('the-ul').appendChild(li);
     }
-    document.getElementById("the-input").value = "";
-    const thePanTag = document.createElement("SPAN");
-    const txt = document.createTextNode("\u00D7");
-    thePanTag.className = "close";
-    thePanTag.appendChild(txt);
+
+    document.getElementById('the-input').value = '';
+    const closeButton = document.createElement('SPAN');
+    const txt = document.createTextNode('\u00D7');
+    closeButton.className = 'close';
+    closeButton.appendChild(txt);
     li.appendChild(thePanTag);
 
-
-    // Removing items when click on SPAN close button
-
-    for (i = 0; i < closeButton.length; i++){
-        closeButton[i].onclick = function () {
-            const theDiv = this.parentElement;
-            theDiv.style.display = "none";
-        }
-    }
+    closeButton.addEventListener('click' , function () {
+        const theDiv = this.parentElement;
+        theDiv.style.display = 'none';
+    });
 }
 
-const input = document.getElementById("the-input");
-const button = document.getElementById("addButton");
+const input = document.getElementById('the-input');
+const button = document.getElementById('addButton');
 
-input.addEventListener('keydown', =>(e) {
-    if (e.keyCode === 13){
+input.addEventListener('keydown', (e) => {
+    if (e.keyCode === 13) {
         createNewElement();
-}
+    }
 });
 
-button.addEventListener('click', =>() {
+button.addEventListener('click', () => {
     createNewElement();
 });
